b06015baaf5466f7322604e60dbd683ece6cc302
Matthew Lombana <mplombana@gmail.com>
2019-05-06 23:15:09 -0400

    Add files via upload

---
+++ cards.py
@@ -0,0 +1,39 @@
+import random
+
+class Card():
+    def __init__(self, suit, value):
+     self.suit = suit
+     self.value = value
+    
+    def show(self):
+        print(self.value, self.suit)
+
+
+class Deck():
+    def __init__(self):
+        self.cards = []
+        self.build()
+
+    def build(self):
+        for s in ["Spades", "Clubs", "Hearts", "Diamonds"]:
+            for v in range(1,14):
+                self.cards.append(Card(s,v))
+
+    def show(self):
+        for c in self.cards:
+            c.show()
+
+    def shuffle(self):
+        for i in range(len(self.cards)-1, 0,-1):
+            r = random.randint(0, i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+
+    def draw(self):
+        return self.cards.pop()
+
+
+deck = Deck()
+deck.shuffle()
+
+card = deck.draw()
+card.show()
